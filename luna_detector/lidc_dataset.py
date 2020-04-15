6f480588d79e8a416f181763c9119e8defd226c6
Maksim Kryuchkov <mkryuchkov@devexperts.com>
2020-04-16 01:40:03 +0300

    minor

--- luna_detector/lidc_dataset.py
+++ luna_detector/lidc_dataset.py
@@ -218,7 +218,7 @@ class LIDCDataset(Dataset):
         #     if len(self.ids) > 500:
         #         break
         with open('index.json', 'r') as read_file:
-            self.ids = json.load(read_file)[:50]
+            self.ids = json.load(read_file)
 
 
 def has_slice_location(dcm_data):
