fe89e3b3d943239b8bf065542db1268097e0ef7c
Maksim Kryuchkov <mkryuchkov@devexperts.com>
2020-04-18 20:56:36 +0300

    minor

--- luna_detector/res18_se.py
+++ luna_detector/res18_se.py
@@ -25,7 +25,7 @@ config['r_rand_crop'] = 0.5
 config['pad_value'] = 0
 config['augtype'] = {'flip': True, 'swap': False, 'scale': True, 'rotate': False}
 config['blacklist'] = ['868b024d9fa388b7ddab12ec1c06af38', '990fbe3f0a1b53878669967b9afd1441',
-                       'adc3bbc63d40f8761c59be10f1e504c3']
+                       'adc3bbc63d40f8761c59be10f1e504c3', 359]
 config['verbose'] = False
 
 
