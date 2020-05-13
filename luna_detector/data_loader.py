a161cb639d3bf4ececf0bb4f829dde9557c57f5e
Maksim Kryuchkov <mkryuchkov@devexperts.com>
2020-05-14 00:28:09 +0300

    minor

--- luna_detector/data_loader.py
+++ luna_detector/data_loader.py
@@ -12,14 +12,9 @@ from torch.utils.data import Dataset
 import os
 import time
 import collections
-import random
-from layers_se import iou
-from scipy.ndimage import zoom
-import warnings
 from scipy.ndimage.interpolation import rotate
-from scipy.ndimage.morphology import binary_dilation, generate_binary_structure
-import pdb
-
+from crop import Crop
+from label_mapping import LabelMapping
 
 def get_filenames_and_labels(idcs, start, end, data_dir):
     new_idcs = []
@@ -84,22 +79,22 @@ class LungNodule3Ddetector(Dataset):
         t = time.time()
         np.random.seed(int(str(t % 1)[2:7]))  # seed according to time
 
-        isRandomImg = False
+        is_random_img = False
         if idx >= len(self.bboxes):
             idx = idx % len(self.bboxes)
-            isRandomImg = np.random.randint(2)
+            is_random_img = np.random.randint(2)
 
-        isRandom = np.random.randint(2)
+        is_random = np.random.randint(2)
 
         if self.phase != 'test':
-            if not isRandomImg:
+            if not is_random_img:
                 bbox = self.bboxes[idx]
                 filename = self.filenames[int(bbox[0])]
                 imgs = np.load(filename)
                 bboxes = self.sample_bboxes[int(bbox[0])]
                 isScale = self.augtype['scale'] and (self.phase == 'train')
-                sample, target, bboxes, coord, _ = self.crop(imgs, bbox[1:], bboxes, isScale, isRandom)
-                if self.phase == 'train' and not isRandom:
+                sample, target, bboxes, coord, _ = self.crop(imgs, bbox[1:], bboxes, isScale, is_random)
+                if self.phase == 'train' and not is_random:
                     sample, target, bboxes, coord = augment(sample, target, bboxes, coord,
                                                             ifflip=self.augtype['flip'],
                                                             ifrotate=self.augtype['rotate'],
@@ -190,254 +185,6 @@ def augment(sample, target, bboxes, coord, ifflip=True, ifrotate=True, ifswap=Tr
                 bboxes[:, ax] = np.array(sample.shape[ax + 1]) - bboxes[:, ax]
     return sample, target, bboxes, coord
 
-
-class Crop(object):
-    def __init__(self, config, random=False):
-        self.crop_size = config['crop_size']
-        self.bound_size = config['bound_size']
-        self.stride = config['stride']
-        self.pad_value = config['pad_value']
-        self.random = random
-
-    def __call__(self, imgs, target, bboxes, isScale=False, isRand=False):
-        """
-        bboxes - array of 4 (3 coord and diameter)
-        target - the original bbox (array of 4)
-        isRand - should we take target into account when creating crop or not
-
-        return:
-        bboxes - array of bboxes, each bbox is an array of 4
-        """
-        # print('crop input bboxes %s' % bboxes)
-        if self.random:
-            np.random.seed(int(time.time() * 1000) % 100000)
-        # print('crop input target %s' % target)
-        if isScale:
-            radiusLim = [8., 100.]
-            scaleLim = [0.75, 1.25]
-            scaleRange = [np.min([np.max([(radiusLim[0] / target[3]), scaleLim[0]]), 1])
-                , np.max([np.min([(radiusLim[1] / target[3]), scaleLim[1]]), 1])]
-            scale = np.random.rand() * (scaleRange[1] - scaleRange[0]) + scaleRange[0]
-            crop_size = (np.array(self.crop_size).astype('float') / scale).astype('int')
-        else:
-            crop_size = self.crop_size
-        bound_size = self.bound_size
-        target = np.copy(target)
-        bboxes = np.copy(bboxes)
-
-        start = []
-        # print('isRand: {}'.format(isRand))
-        # print('target (in crop call method): {}'.format(target))
-        for i in range(3):
-            if not isRand:
-                r = target[3] / 2
-                s = np.floor(target[i] - r) + 1 - bound_size
-                e = np.ceil(target[i] + r) + 1 + bound_size - crop_size[i]
-            else:
-                s = np.max([imgs.shape[i + 1] - crop_size[i] / 2, imgs.shape[i + 1] / 2 + bound_size])
-                e = np.min([crop_size[i] / 2, imgs.shape[i + 1] / 2 - bound_size])
-                target = np.array([np.nan, np.nan, np.nan, np.nan])
-            #     randomized crops including target (or not including depending on isRand
-            if s > e:
-                start.append(int(np.random.randint(e, s)))  # !
-                # print('start append {}'.format(start[-1]))
-            else:
-                start.append(int(target[i] - crop_size[i] / 2 + np.random.randint(-bound_size / 2, bound_size / 2)))
-            # print(s, e, bound_size)
-
-        # print('start %s' % start)
-        normstart = np.array(start).astype('float32') / np.array(imgs.shape[1:]) - 0.5
-        normsize = np.array(crop_size).astype('float32') / np.array(imgs.shape[1:])
-        # print('normstart %s' % normstart)
-        # print('normsize %s' % normsize)
-        xx, yy, zz = np.meshgrid(
-            np.linspace(normstart[0], normstart[0] + normsize[0], self.crop_size[0] // self.stride),
-            np.linspace(normstart[1], normstart[1] + normsize[1], self.crop_size[1] // self.stride),
-            np.linspace(normstart[2], normstart[2] + normsize[2], self.crop_size[2] // self.stride),
-            indexing='ij')
-        coord = np.concatenate([xx[np.newaxis, ...], yy[np.newaxis, ...], zz[np.newaxis, :]], 0).astype('float32')
-
-        pad = [[0, 0]]
-        for i in range(3):
-            leftpad = max(0, -start[i])
-            rightpad = max(0, start[i] + crop_size[i] - imgs.shape[i + 1])
-            pad.append([leftpad, rightpad])
-        crop = imgs[:,
-               max(start[0], 0):min(start[0] + crop_size[0], imgs.shape[1]),
-               max(start[1], 0):min(start[1] + crop_size[1], imgs.shape[2]),
-               max(start[2], 0):min(start[2] + crop_size[2], imgs.shape[3])
-               ]
-        crop = np.pad(crop, pad, 'constant', constant_values=self.pad_value)
-        for i in range(3):
-            target[i] = target[i] - start[i]
-        for i in range(len(bboxes)):
-            for j in range(3):
-                bboxes[i][j] = bboxes[i][j] - start[j]
-
-        if isScale:
-            with warnings.catch_warnings():
-                warnings.simplefilter("ignore")
-                crop = zoom(crop, [1, scale, scale, scale], order=1)
-            newpad = self.crop_size[0] - crop.shape[1:][0]
-            if newpad < 0:
-                crop = crop[:, :-newpad, :-newpad, :-newpad]
-            elif newpad > 0:
-                pad2 = [[0, 0], [0, newpad], [0, newpad], [0, newpad]]
-                crop = np.pad(crop, pad2, 'constant', constant_values=self.pad_value)
-            for i in range(4):
-                target[i] = target[i] * scale
-            for i in range(len(bboxes)):
-                for j in range(4):
-                    bboxes[i][j] = bboxes[i][j] * scale
-        if isRand:
-            target = np.array([np.nan, np.nan, np.nan, np.nan])
-        return crop, target, bboxes, coord, 1
-
-
-class LabelMapping(object):
-    def __init__(self, config, phase):
-        self.stride = np.array(config['stride'])
-        self.num_neg = int(config['num_neg'])
-        self.th_neg = config['th_neg']
-        self.anchors = np.asarray(config['anchors'])
-        self.phase = phase
-        if phase == 'train':
-            self.th_pos = config['th_pos_train']
-        elif phase == 'val':
-            self.th_pos = config['th_pos_val']
-
-    def __call__(self, input_size, target, bboxes):
-        stride = self.stride
-        num_neg = self.num_neg
-        th_neg = self.th_neg
-        anchors = self.anchors
-        th_pos = self.th_pos
-        struct = generate_binary_structure(3, 1)
-
-        output_size = []
-        for i in range(3):
-            assert (input_size[i] % stride == 0)
-            output_size.append(int(input_size[i] / stride))
-
-        label = np.zeros(output_size + [len(anchors), 5], np.float32)
-        offset = ((stride.astype('float')) - 1) / 2
-        oz = np.arange(offset, offset + stride * (output_size[0] - 1) + 1, stride)
-        oh = np.arange(offset, offset + stride * (output_size[1] - 1) + 1, stride)
-        ow = np.arange(offset, offset + stride * (output_size[2] - 1) + 1, stride)
-
-        for bbox in bboxes:
-            for i, anchor in enumerate(anchors):
-                iz, ih, iw = select_samples(bbox, anchor, th_neg, oz, oh, ow)
-                label[iz, ih, iw, i, 0] = 1
-                label[:, :, :, i, 0] = binary_dilation(label[:, :, :, i, 0].astype('bool'), structure=struct,
-                                                       iterations=1).astype('float32')
-
-        label = label - 1
-
-        if self.phase == 'train' and self.num_neg > 0:
-            neg_z, neg_h, neg_w, neg_a = np.where(label[:, :, :, :, 0] == -1)
-            neg_idcs = random.sample(range(len(neg_z)), min(num_neg, len(neg_z)))
-            neg_z, neg_h, neg_w, neg_a = neg_z[neg_idcs], neg_h[neg_idcs], neg_w[neg_idcs], neg_a[neg_idcs]
-            label[:, :, :, :, 0] = 0
-            label[neg_z, neg_h, neg_w, neg_a, 0] = -1
-
-        # if the whole sample is negative then target[0] is nan so no further label mapping?
-        if np.isnan(target[0]):
-            return label
-        iz, ih, iw, ia = [], [], [], []
-        for i, anchor in enumerate(anchors):
-            iiz, iih, iiw = select_samples(target, anchor, th_pos, oz, oh, ow)
-            iz.append(iiz)
-            ih.append(iih)
-            iw.append(iiw)
-            ia.append(i * np.ones((len(iiz),), np.int64))
-        iz = np.concatenate(iz, 0)
-        ih = np.concatenate(ih, 0)
-        iw = np.concatenate(iw, 0)
-        ia = np.concatenate(ia, 0)
-        flag = True
-        if len(iz) == 0:
-            pos = []
-            for i in range(3):
-                pos.append(max(0, int(np.round((target[i] - offset) / stride))))
-            idx = np.argmin(np.abs(np.log(target[3] / anchors)))
-            pos.append(idx)
-            flag = False
-        else:
-            idx = random.sample(range(len(iz)), 1)[0]
-            pos = [iz[idx], ih[idx], iw[idx], ia[idx]]
-        dz = (target[0] - oz[pos[0]]) / anchors[pos[3]]
-        dh = (target[1] - oh[pos[1]]) / anchors[pos[3]]
-        dw = (target[2] - ow[pos[2]]) / anchors[pos[3]]
-        dd = np.log(target[3] / anchors[pos[3]])
-        label[pos[0], pos[1], pos[2], pos[3], :] = [1, dz, dh, dw, dd]
-        return label
-
-
-def select_samples(bbox, anchor, th, oz, oh, ow):
-    z, h, w, d = bbox
-    max_overlap = min(d, anchor)
-    min_overlap = np.power(max(d, anchor), 3) * th / max_overlap / max_overlap
-    if min_overlap > max_overlap:
-        return np.zeros((0,), np.int64), np.zeros((0,), np.int64), np.zeros((0,), np.int64)
-    else:
-        s = z - 0.5 * np.abs(d - anchor) - (max_overlap - min_overlap)
-        e = z + 0.5 * np.abs(d - anchor) + (max_overlap - min_overlap)
-        mz = np.logical_and(oz >= s, oz <= e)
-        iz = np.where(mz)[0]
-
-        s = h - 0.5 * np.abs(d - anchor) - (max_overlap - min_overlap)
-        e = h + 0.5 * np.abs(d - anchor) + (max_overlap - min_overlap)
-        mh = np.logical_and(oh >= s, oh <= e)
-        ih = np.where(mh)[0]
-
-        s = w - 0.5 * np.abs(d - anchor) - (max_overlap - min_overlap)
-        e = w + 0.5 * np.abs(d - anchor) + (max_overlap - min_overlap)
-        mw = np.logical_and(ow >= s, ow <= e)
-        iw = np.where(mw)[0]
-
-        if len(iz) == 0 or len(ih) == 0 or len(iw) == 0:
-            return np.zeros((0,), np.int64), np.zeros((0,), np.int64), np.zeros((0,), np.int64)
-
-        lz, lh, lw = len(iz), len(ih), len(iw)
-        iz = iz.reshape((-1, 1, 1))
-        ih = ih.reshape((1, -1, 1))
-        iw = iw.reshape((1, 1, -1))
-        iz = np.tile(iz, (1, lh, lw)).reshape((-1))
-        ih = np.tile(ih, (lz, 1, lw)).reshape((-1))
-        iw = np.tile(iw, (lz, lh, 1)).reshape((-1))
-        centers = np.concatenate([
-            oz[iz].reshape((-1, 1)),
-            oh[ih].reshape((-1, 1)),
-            ow[iw].reshape((-1, 1))], axis=1)
-
-        r0 = anchor / 2
-        s0 = centers - r0
-        e0 = centers + r0
-
-        r1 = d / 2
-        s1 = bbox[:3] - r1
-        s1 = s1.reshape((1, -1))
-        e1 = bbox[:3] + r1
-        e1 = e1.reshape((1, -1))
-
-        overlap = np.maximum(0, np.minimum(e0, e1) - np.maximum(s0, s1))
-
-        intersection = overlap[:, 0] * overlap[:, 1] * overlap[:, 2]
-        union = anchor * anchor * anchor + d * d * d - intersection
-
-        iou = intersection / union
-
-        mask = iou >= th
-        # if th > 0.4:
-        #   if np.sum(mask) == 0:
-        #      print(['iou not large', iou.max()])
-        # else:
-        #    print(['iou large', iou[mask]])
-        iz = iz[mask]
-        ih = ih[mask]
-        iw = iw[mask]
-        return iz, ih, iw
 
 
 def collate(batch):
--- luna_detector/test.py
+++ luna_detector/test.py
@@ -6,27 +6,27 @@ import numpy as np
 import torch
 from data_loader import LungNodule3Ddetector
 from layers_se import *
-from lidc_dataset import LIDCDataset
+from patient_data_loader import PatientDataLoader
 from torch.utils.data import DataLoader
+from abc import abstractmethod, ABC
 
 from config_training import config as config_training
 
-
-class Test:
-    def __init__(self, data_path=None, thr=0, path_to_model='', start=0, end=0, iou_threshold=0.5, r_rand=0.9):
-        if data_path is None:
-            self.data_path = os.path.join('/content/drive/My Drive/DeepSEED-3D-ConvNets-for-Pulmonary-Nodule-Detection',
+default_data_path = os.path.join('/content/drive/My Drive/DeepSEED-3D-ConvNets-for-Pulmonary-Nodule-Detection',
                                    config_training['preprocess_result_path'])
-        else:
-            self.data_path = data_path
-        self.base_path = '/content/drive/My Drive/DeepSEED-3D-ConvNets-for-Pulmonary-Nodule-Detection'
-        self.luna_path = opjoin(self.base_path, 'luna_detector')
+base_path = '/content/drive/My Drive/DeepSEED-3D-ConvNets-for-Pulmonary-Nodule-Detection'
+luna_path = opjoin(base_path, 'luna_detector')
+
+
+class AbstractTest:
+    def __init__(self, data_path=None, thr=0, path_to_model='', start=0, end=0, iou_threshold=0.5):
+        self.data_path = default_data_path if data_path is None else data_path
         model = import_module('res18_se')
         print('creating model')
         config, net, loss, get_pbb = model.get_model()
         net = net.cuda()
         loss = loss.cuda()
-        checkpoint = torch.load(opjoin(self.luna_path, 'test_results', path_to_model))
+        checkpoint = torch.load(opjoin(luna_path, 'test_results', path_to_model))
         net.load_state_dict(checkpoint['state_dict'])
         self.net = net
         self.iou_threshold = iou_threshold
@@ -36,76 +36,90 @@ class Test:
         self.thr = thr
         self.start = start
         self.end = end
-
-        luna_test = np.load('./luna_test.npy')
-        dataset = LungNodule3Ddetector(self.data_path, luna_test, self.config, start=0, end=0, r_rand=r_rand)
+        dataset = self.create_dataset()
         data_loader = DataLoader(dataset, batch_size=1, shuffle=False, num_workers=1,
                                  pin_memory=True)
-        self.outputs = []
-        self.targets = []
-        for i, tpl in enumerate(data_loader):
-            data = torch.autograd.Variable(tpl[0].cuda())
-            target = torch.autograd.Variable(tpl[1].cuda())
-            coord = torch.autograd.Variable(tpl[2].cuda())
+
+        self.outputs, self.targets = self.predict_on_data(data_loader)
+
+    @abstractmethod
+    def create_dataset(self):
+        pass
+
+    def predict_on_data(self, data_loader):
+        outputs, targets = [], []
+        for i, data, target, coord in enumerate(data_loader):
+            data, target, coord = data.cuda(), target.cuda(), coord.cuda()
             data = data.type(torch.cuda.FloatTensor)
             coord = coord.type(torch.cuda.FloatTensor)
             print(data.shape)
             print(coord.shape)
 
             output = self.net(data, coord)
-            self.outputs.append(output.cpu().detach().numpy()[0])
-            self.targets.append(target.cpu().detach().numpy()[0])
+            outputs.append(output.cpu().detach().numpy()[0])
+            targets.append(target)
+        return outputs, [self.transform_target(targets)]
 
+    @abstractmethod
+    def transform_target(self, target):
+        pass
 
+    @abstractmethod
+    def is_positive(self, target):
+        pass
 
-    def test_luna(self, thr, iou_threshold):
-        return self.common_test(thr=thr, iou_threshold=iou_threshold)
-
-    def common_test(self, thr, iou_threshold):
-        tn = 0
-        tp = 0
-        n = 0
-        p = 0
-        fp_bbox = 0
-        total_pred_len = 0
-        dices = []
-        target_volumes = []
-        output_volumes = []
+    def common_test(self, threshold):
+        tn, tp, n, p = 0, 0, 0, 0
         for output, target in zip(self.outputs, self.targets):
-
-            pred = self.gp(output, thr)
+            pred = self.gp(output, threshold)
             true = self.gp(target, 0.8)
-            current_dice = 0
-            if len(true) > 0:
+            if self.is_positive(target):
                 p += 1
-                correct_positive = 0
-                current_dices = []
-                for bbox in pred:
-                    cdice = dice(bbox[1:], true[0][1:])
-                    if cdice > iou_threshold:
-                        correct_positive = 1
-                    else:
-                        fp_bbox += 1
-                    # print('dice: {}'.format(cdice))
-                    current_dices.append([bbox[4], dice(bbox[1:], true[0][1:])])
-                if len(pred) == 0:
-                    current_dices.append([-1, 0])
-                current_dices = np.array(current_dices)
                 if len(pred) > 0:
                     tp += 1
-                max_dice_idx = np.argmax(current_dices[:, 1])
-                current_dice = current_dices[max_dice_idx][1]
-                r = current_dices[max_dice_idx][0]
-                dices.append(current_dice)
-                target_volumes.append(true[0][4])
-                output_volumes.append(r)
-                total_pred_len += len(pred)
             else:
                 n += 1
                 if len(pred) == 0:
                     tn += 1
-
             print('pred: {}'.format(pred))
             print('true: {}'.format(true))
-            print(tp, tn, p, n, fp_bbox, current_dice)
-        return [tp, tn, p, n, fp_bbox, total_pred_len], dices
+            print(tp, tn, p, n)
+        return [tp, tn, p, n]
+
+
+
+class SimpleTest(AbstractTest):
+
+    def create_dataset(self):
+        luna_test = np.load('./luna_test.npy')
+        dataset = LungNodule3Ddetector(self.data_path, luna_test, self.config, start=0, end=0, r_rand=0.9)
+        return dataset
+
+
+    def is_positive(self, target):
+        return len(self.gp(target, 0.8)) > 0
+
+    def transform_target(self, target):
+        return target.cpu().detach().numpy()[0]
+
+    def test_luna(self, threshold):
+        return self.common_test(threshold=threshold)
+
+
+class PatientTest(AbstractTest):
+
+    def create_dataset(self):
+        luna_test = np.load('./luna_test.npy')
+        dataset = PatientDataLoader(self.data_path, luna_test, self.config, start=0, end=0)
+        return dataset
+
+    def transform_target(self, target):
+        return target.cpu().detach().numpy()[0]
+
+
+    def is_positive(self, target):
+        return target
+
+    def test_luna(self, threshold):
+        return self.common_test(threshold=threshold)
+
