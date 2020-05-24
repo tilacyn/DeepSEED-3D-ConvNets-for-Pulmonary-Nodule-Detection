01a0bd65079cf9f8cc96b52cfe8782f1d3823d34
Maksim Kryuchkov <mkryuchkov@devexperts.com>
2020-05-24 18:39:01 +0300

    added paths file

---
+++ luna_detector/path.py
@@ -0,0 +1,7 @@
+from os.path import join as opjoin
+
+mydrive_path = '/content/drive/My Drive'
+base_path = opjoin(mydrive_path, 'DeepSEED-3D-ConvNets-for-Pulmonary-Nodule-Detection')
+augmented_data_path = opjoin(mydrive_path, 'CT-GAN')
+data_path = '/content/drive/My Drive/dsb2018_topcoders/data'
+luna_path = opjoin(base_path, 'luna_detector')
